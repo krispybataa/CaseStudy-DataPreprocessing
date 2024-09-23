2c880d6efcefc9583ae15c3368a0667401ff85ec
Clark Rodriguez <acrprodriguez05@gmail.com>
2024-09-23 16:18:04 +0800

    Rounding of values to 4 decimal places

--- preprocessing.py
+++ preprocessing.py
@@ -11,7 +11,7 @@ df = pd.read_csv(path)
 
 # Descriptive Statistics for Age
 print(f"--- AGE STATISTICS ---")
-age_stats = df['age'].describe()
+age_stats = df['age'].describe().round(4)
 age_min = df['age'].min()
 age_max = df['age'].max()
 age_range = age_max - age_min
@@ -40,23 +40,23 @@ print(f"\n{'-'*40}\n")
 
 # Pivot report on the relationship between Civil Status and Number of Children
 print(f"--- PIVOT REPORT: MARRIED VS. NUMBER OF CHILDREN ---")
-pivot_report = df.pivot_table(index='married', values='children', aggfunc='mean')
+pivot_report = df.pivot_table(index='married', values='children', aggfunc='mean').round(4)
 print(pivot_report)
 print(f"\n{'-'*40}\n")
 
 # Calculation of means of Age, Income, and Children by PEP, Married, and Has Car
-print(f"--- MEANS OF AGE, INCOME, AND CHILDREN (BY PEP, MARRIED, AND CAR) ---")
-grouped_means = df.groupby(['pep', 'married', 'car'])[['age', 'income', 'children']].mean()
+print(f"--- GROUPED MEANS ---")
+grouped_means = df.groupby(['pep', 'married', 'car'])[['age', 'income', 'children']].mean().round(4)
 print(grouped_means)
 print(f"\n{'-'*40}\n")
 
 # Pattern analysis for PEP purchase and Civil status
 print(f"--- PATTERN ANALYSIS: AGE AND PEP ---")
-pep_age_pattern = df.groupby('pep')['age'].mean()
+pep_age_pattern = df.groupby('pep')['age'].mean().round(4)
 print(pep_age_pattern)
 
 print(f"\n--- PATTERN ANALYSIS: NUMBER OF CHILDREN, PEP, AND MARRIAGE ---")
-pep_children_pattern = df.groupby(['pep', 'married'])['children'].mean()
+pep_children_pattern = df.groupby(['pep', 'married'])['children'].mean().round(4)
 print(pep_children_pattern)
 print(f"\n{'-'*40}\n")
 
@@ -68,14 +68,14 @@ print(f"~ SECTION ON DATA TRANSFORMATION ~\n")
 
 # Normalizing Income into [0,1] scale
 scaler = MinMaxScaler()
-df['income_Normalized'] = scaler.fit_transform(df[['income']])
+df['income_Normalized'] = scaler.fit_transform(df[['income']]).round(4)
 
 print(f"--- NORMALIZED INCOME COLUMN ---")
 print(df[['income', 'income_Normalized']].head())
 print(f"\n{'-'*40}\n")
 
 # Equal Depth (frequency) binning
-df['income_Binned'] = pd.qcut(df['income'], q=3, labels=["Low", "Medium", "High"])
+df['income_Binned'] = pd.qcut(df['income'], q=3, labels=["Low", "Medium", "High"]).round(4)
 
 print(f"--- BINNED INCOME COLUMN ---")
 print(df[['income', 'income_Binned']].head())
